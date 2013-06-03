69295b21812373c2571648f62c07d596aa2c0866
psmagicman <julienlaw1@gmail.com>
2013-06-03 12:41:08 -0700

    users data is now processed in the PHP. Will create a table to display the data in users.php

---
+++ lib/class.network-stats-users-data.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ *	Network Stats Users Data class file
+ */
+
+class Network_Stats_Users_Data extends Network_Stats_Data {
+
+	function __construct( $name = 'users') {
+		$this->name = $name;
+		$this->start();
+	}
+
+	/**
+	 * fetch_latest_data function
+	 * This function will create/update the JSON file associated with the current view
+	 * @access public
+	 * @return void
+	 */
+	function fetch_latest_data() {
+
+		global $wpdb;
+
+		$blogs = $wpdb->get_results( "SELECT blog_id, domain, path FROM $wpdb->blogs ORDER BY domain ASC" );
+
+		if( $blogs ) {
+
+			foreach( $blogs as $blog ) {
+				$blog_users = get_users( 'blog_id=' . $blog->blog_id );	// generates an array of users for each blog
+
+				$this->append_to_data( array(
+					'users'		=> $blog_users,
+					'site_id'	=> $blog->blog_id,
+					'site_url' 	=> 'http://' . $blog->domain . $blog->path
+				) );
+			}
+
+		}
+
+		$this->update();
+
+	}
+
+	/**
+	 * generate_users_data function
+	 * This function will generate an array containing the necessary information gathered from the blog users
+	 * @access public
+	 * @param array
+	 * @return array
+	 */
+	function generate_users_data( $decoded_json ) {
+		foreach( $decoded_json as $key => $sub_arr ) {
+			$users = $this->grab_users_data($sub_arr['users']);
+			//print_r( $users );
+			//echo '<hr/>';
+			$filtered_data[] = array(
+				'data' => $users,
+				'site_url' => $sub_arr['site_url']
+			);
+		}
+	}
+
+	/**
+	 * grab_users_data function
+	 * This function will parse through the users sub-array and grab the data needed from it
+	 * @access private
+	 * @param array
+	 * @return array
+	 */
+	private function grab_users_data( $users_array ) {
+		foreach( $users_array as $key => $value ) {
+			$grabbed_data[] = array(
+				'email' => $value['data']['user_email'],
+				'role' => $value['roles'][0],
+				'registered' => $value['data']['user_registered']
+			);
+			/*echo "email: " . $value['data']['user_email'] . "<br/>";
+			echo "registered: " . $value['data']['user_registered'] . "<br/>"; 
+			echo "role: " . $value['roles'][0] . "<br/>";
+			echo '<br/><br/>'; */
+		}
+		return $grabbed_data;
+	}
+}
--- views/users.php
+++ views/users.php
@@ -1,1 +1,17 @@
-coming soon
+<?php
+
+$users_data_object = new Network_Stats_Users_Data();
+$url = $users_data_object->get_url();
+
+if( empty( $url ) ) {
+
+	$users_data_object->fetch_latest_data();
+
+}
+
+$users_json_data = $users_data_object->get();
+$users_array = $users_data_object->generate_users_data( $users_json_data );
+
+?>
+
+<p>Last updated <em><?php echo $users_data_object->updated_since(); ?></em></p>
