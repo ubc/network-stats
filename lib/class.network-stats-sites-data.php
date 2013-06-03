979e15ea6518310c2d7d6f1cb4732dfa0629900a
Enej Bajgoric <enej.bajgoric@gmail.com>
2013-06-03 12:40:33 -0700

    Updates sites stats

--- lib/class.network-stats-sites-data.php
+++ lib/class.network-stats-sites-data.php
@@ -1,6 +1,8 @@
 <?php 
 
 
+add_action('wp_ajax_network_stats_site_data', array( 'Network_Stats_Sites_Data','fetch_latest_data' ) );
+
 class Network_Stats_Sites_Data extends Network_Stats_Data {
 	
 	function __construct( $name = 'sites' ) {
@@ -8,32 +10,16 @@ class Network_Stats_Sites_Data extends Network_Stats_Data {
 		$this->start();
 	}
 	
-	function fetch_latest_data(){
-		global $wpdb;
+	function fetch_latest_data( ) {
+	
 		
-		$blogs  = $wpdb->get_results("SELECT blog_id, domain, path FROM $wpdb->blogs ORDER BY domain ASC");
-		
-		if ( $blogs ) {
-			foreach ($blogs as $blog) {
-				
-				$theme = $wpdb->get_col("SELECT option_value FROM ".$wpdb->base_prefix.$blog->blog_id."_options WHERE option_name = 'current_theme'");
-				$theme_name = ( empty( $theme[0] ) ? '' : $theme[0]);
-				$this->append_to_data( array(
-					'theme' => $theme[0],
-					'site_id' => $blog->blog_id,
-					'site_url' => 'http://'.$blog->domain.$blog->path
-				) );
-			}
-		}
-		// do stats
-		if( $blogs ) {
-			foreach ($blogs as $blog) {
-				
-				
-			}
-		}
-		
-		
-		$this->update();
 	}
+	
+	function temp_append_to_data() {
+		
+	
+	}
+	
+	
+	
 }
